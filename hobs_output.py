72478beb050ee62cd1908008b0d8045f3555faa1
Joshua Larsen <jlarsen@usgs.gov>
2018-07-25 15:44:19 -0700

    Added base classes and example usage to repository.

---
+++ hobs_output.py
@@ -0,0 +1,541 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+
+class HobsHeader(object):
+    sim_head = '"SIMULATED EQUIVALENT"'
+    obs_head = '"OBSERVED VALUE"'
+    obs_name = '"OBSERVATION NAME"'
+    date = 'DATE'
+    dyear = 'DECIMAL_YEAR'
+
+    header = {sim_head: None,
+              obs_head: None,
+              obs_name: None,
+              date: None,
+              dyear : None}
+
+
+class HobsOut(dict):
+    """
+    Reads output data from Hobs file and prepares it for post processing.
+
+    Class sets observations to an ordered dictionary based on observation name
+
+    If observation name is consistant for a site, a time series is created
+    for plotting!
+    """
+
+    def __init__(self, filename):
+        super(HobsOut, self).__init__()
+        self.name = filename
+
+        self.__read_hobs_output()
+
+
+    def __read_hobs_output(self):
+        """
+        Method to read a hobs output file. Dynamically sets header information
+        and reads associated values.
+
+        Sets values to HobsOut dictionary
+        """
+
+        with open(self.name) as hobout:
+            for ix, line in enumerate(hobout):
+                if ix == 0:
+                    self.__set_header(line)
+
+                else:
+                    self.__set_dictionary_values(line)
+
+    def __set_dictionary_values(self, line):
+        """
+        Method to set incoming hobs line to dictionary data values
+
+        Args:
+            line: (str)
+        """
+        t = line.strip().split()
+        obsname = t[HobsHeader.header[HobsHeader.obs_name]]
+        simval = float(t[HobsHeader.header[HobsHeader.sim_head]])
+        obsval = float(t[HobsHeader.header[HobsHeader.obs_head]])
+        residual = simval - obsval
+        date = self.__set_datetime_object(t[HobsHeader.header[HobsHeader.date]])
+        decimal_date = float(t[HobsHeader.header[HobsHeader.dyear]])
+
+        if obsname in self:
+            self[obsname]['simval'].append(simval)
+            self[obsname]['obsval'].append(obsval)
+            self[obsname]['date'].append(date)
+            self[obsname]['decimal_date'].append(decimal_date)
+            self[obsname]['residual'].append(residual)
+        else:
+            self[obsname] = {"simval": [simval], "obsval": [obsval],
+                             "date": [date], "decimal_date": [decimal_date],
+                             "residual": [residual]}
+
+    def __set_header(self, line):
+        """
+        Reads header line and sets header index
+
+        Args:
+            line:
+
+        Returns:
+
+        """
+        n = 0
+        s = ""
+
+        for i in line:
+            s += (i)
+            if s in HobsHeader.header:
+                HobsHeader.header[s] = n
+                n += 1
+                s = ""
+
+            elif s in (" ", "\t", "\n"):
+                s = ""
+
+            else:
+                pass
+
+        for key, value in HobsHeader.header.items():
+            if value is None:
+                raise AssertionError("HobsHeader headings must be updated")
+
+    def __set_datetime_object(self, s):
+        """
+        Reformats a string of YYYY-mm-dd to a datetime object
+
+        Args:
+            s: (str) string of YYYY-mm-dd
+
+        Returns:
+            datetime.date
+        """
+        return dt.datetime.strptime(s, "%Y-%m-%d")
+
+    def __get_date_string(self, date):
+        """
+
+        Args:
+            datetime:
+
+        Returns:
+
+        """
+        return date.strftime("%Y/%m/%d")
+
+    def get_sum_squared_errors(self, obsname):
+        """
+        Returns the sum of squared errors from the residual
+
+        Args:
+            obsname: (str)
+
+        Returns:
+        """
+        return sum([i**2 for i in self[obsname]['residual']])
+
+    def get_rmse(self, obsname):
+        """
+        Returns the RMSE from the residual
+
+        Args:
+            obsname: (str)
+
+        Returns:
+        """
+        return np.sqrt(np.mean([i**2 for i in self[obsname]['residual']]))
+
+    def get_number_observations(self, obsname):
+        """
+        Returns the number of observations for an obsname
+
+        Args:
+            obsname: (str)
+
+        Returns:
+        """
+        return len(self[obsname]['simval'])
+
+    def get_maximum_residual(self, obsname):
+        """
+        Returns the datetime.date and maximum residual value
+
+        Args:
+            obsname: (str)
+
+        Returns:
+            tuple: (datetime.date, residual)
+        """
+        data = self[obsname]['residual']
+        index = data.index(max(data))
+        date = self[obsname]['date'][index]
+        return (date, max(data))
+
+    def get_minimum_residual(self, obsname):
+        """
+        Returns the datetime.date, minimum residual value
+
+        Args:
+            obsname: (str)
+
+        Returns:
+            tuple: (datetime.date, residual)
+        """
+        data = self[obsname]['residual']
+        index = data.index(min(data))
+        date = self[obsname]['date'][index]
+        return (date, min(data))
+
+    def get_mean_residual(self, obsname):
+        """
+        Returns the datetime.date, minimum residual value
+
+        Args:
+            obsname: (str)
+
+        Returns:
+            tuple: (datetime.date, residual)
+        """
+        data = self[obsname]['residual']
+        return np.mean(data)
+
+    def get_median_residual(self, obsname):
+        """
+        Returns the datetime.date, minimum residual value
+
+        Args:
+            obsname: (str)
+
+        Returns:
+            tuple: (datetime.date, residual)
+        """
+        data = self[obsname]['residual']
+        return np.median(data)
+
+    def get_maximum_residual_heads(self, obsname):
+        """
+        Returns the datetime.date, simulated, and observed
+        heads at the maximum residual value
+        Args:
+            obsname: (str)
+
+        Returns:
+            tuple: (datetime.date, simulated head, observed head)
+        """
+        resid = self[obsname]['residual']
+        index = resid.index(max(resid))
+        observed = self[obsname]['obsval'][index]
+        simulated = self[obsname]['simval'][index]
+        date = self[obsname]['date'][index]
+        return (date, simulated, observed)
+
+    def get_minimum_residual_heads(self, obsname):
+        """
+        Returns the datetime.date, simulated, and observed
+        heads at the maximum residual value
+        Args:
+            obsname: (str)
+
+        Returns:
+            tuple: (datetime.date, simulated head, observed head)
+        """
+        resid = self[obsname]['residual']
+        index = resid.index(min(resid))
+        observed = self[obsname]['obsval'][index]
+        simulated = self[obsname]['simval'][index]
+        date = self[obsname]['date'][index]
+        return (date, simulated, observed)
+
+    def get_residual_bias(self, filter=None):
+        """
+        Method to determine the bias of measurements +-
+        by checking the residual. Returns fraction of residuals
+        > 0.
+        Args:
+            filter: (str, list, tuple, or function)
+                filtering criteria for writing statistics.
+                Function must return True for filter out, false to use
+
+        Returns:
+            (float) fraction of residuals greater than zero
+        """
+        nobs = 0.
+        ngreaterzero = 0.
+
+        for obsname, meta_data in self.items():
+            if self.__filter(obsname, filter):
+                continue
+
+            residual = np.array(meta_data['residual'])
+            rgreaterzero = sum((residual > 0))
+
+            nobs += residual.size
+            ngreaterzero += rgreaterzero
+
+        try:
+            bias = ngreaterzero / nobs
+        except ZeroDivisionError:
+            raise ZeroDivisionError("No observations found!")
+
+        return bias
+
+    def write_dbf(self, dbfname, filter=None):
+        """
+        Method to write a dbf file from a the HOBS dictionary
+
+        Args:
+            dbfname: (str) dbf file name
+        """
+        import shapefile
+        data = []
+        for obsname, meta_data in self.items():
+
+            if self.__filter(obsname, filter):
+                continue
+
+            for ix, val in enumerate(meta_data['simval']):
+                data.append([obsname,
+                             self.__get_date_string(meta_data['date'][ix]),
+                             val,
+                             meta_data['obsval'][ix],
+                             meta_data['residual'][ix]])
+
+        w = shapefile.Writer()
+        w.field("HOBSNAME", fieldType="C")
+        w.field("HobsDate", fieldType="D")
+        w.field("HeadSim", fieldType='N', decimal=8)
+        w.field("HeadObs", fieldType="N", decimal=8)
+        w.field("Residual", fieldType="N", decimal=8)
+
+        for rec in data:
+            w.record(*rec)
+
+        w.save(dbf=dbfname)
+
+    def write_min_max_residual_dbf(self, dbfname, filter=None):
+        """
+        Method to write a dbf of transient observations
+        using observation statistics
+
+        Args:
+            dbfname: (str) dbf file name
+
+        """
+        import shapefile
+        data = []
+        for obsname, meta_data in self.items():
+            if self.__filter(obsname, filter):
+                continue
+
+            max_date, resid_max = self.get_maximum_residual(obsname)
+            min_date, resid_min = self.get_minimum_residual(obsname)
+            simval_max, obsval_max = self.get_maximum_residual_heads(obsname)[1:]
+            simval_min, obsval_min = self.get_minimum_residual_heads(obsname)[1:]
+
+            data.append([obsname,
+                         self.get_number_observations(obsname),
+                         self.__get_date_string(max_date), resid_max,
+                         self.__get_date_string(min_date), resid_min,
+                         simval_max, obsval_max, simval_min, obsval_min])
+
+        w = shapefile.Writer()
+        w.field("HOBSNAME", fieldType="C")
+        w.field("FREQUENCY", fieldType="N")
+        w.field("MaxDate", fieldType="C")
+        w.field("MaxResid", fieldType='N', decimal=8)
+        w.field("MinDate", fieldType="C", decimal=8)
+        w.field("MinResid", fieldType="N", decimal=8)
+        w.field("MaxHeadSim", fieldType="N", decimal=8)
+        w.field("MaxHeadObs", fieldType="N", decimal=8)
+        w.field("MinHeadSim", fieldType="N", decimal=8)
+        w.field("MinHeadObs", fieldType="N", decimal=8)
+
+        for rec in data:
+            w.record(*rec)
+
+        w.save(dbf=dbfname)
+
+    def __filter(self, obsname, filter):
+        """
+        Boolean filetering method, checks if observation name
+        is in the filter.
+
+        Args:
+            filter: (str, list, tuple, or function)
+                filtering criteria for writing statistics.
+                Function must return True for filter out, false for write to file
+
+        Returns:
+            bool: True if obsname in filter
+        """
+        if filter is None:
+            return False
+
+        elif isinstance(filter, list) or isinstance(filter, tuple):
+            if obsname in list:
+                return True
+
+        elif isinstance(filter, str):
+            if obsname == filter:
+                return True
+
+        elif callable(filter):
+            if filter(obsname):
+                return True
+
+        else:
+            raise Exception("Filter is not an appropriate type")
+
+        return False
+
+    def write_summary_statistics_csv(self, csvname, filter=None):
+        """
+        Method to write summary calibration statistics to a
+        CSV file for analysis and reports
+
+        Args:
+            csvname: (str) csv file name
+            filter: (str, list, tuple, or function)
+                filtering criteria for writing statistics.
+                Function must return True for filter out, false for write to file
+
+        """
+        data = []
+        header = ["Well name", "Average", "Median",
+                  "Minimum", "Maximum", "RMSE ft", "Frequency"]
+
+        for obsname, meta_data in sorted(self.items()):
+
+            if self.__filter(obsname, filter):
+                continue
+
+            resid_mean = self.get_mean_residual(obsname)
+            resid_median = self.get_median_residual(obsname)
+            resid_max = self.get_maximum_residual(obsname)[-1]
+            resid_min = self.get_minimum_residual(obsname)[-1]
+            rmse = self.get_rmse(obsname)
+            frequency = self.get_number_observations(obsname)
+            data.append((obsname, resid_mean, resid_median,
+                         resid_min, resid_max, rmse, frequency))
+
+        data = np.array(data, dtype=[('id', 'S15'), ('mean', float),
+                                     ('med', float), ('min', float),
+                                     ('max', float), ('rmse', float),
+                                     ('num', np.int)])
+
+        with open(csvname, "w") as foo:
+            foo.write(",".join(header) + "\n")
+            np.savetxt(foo, data, fmt="%15s,%.2f,%2f,%2f,%2f,%2f,%d")
+
+    def plot(self, obsname, *args, **kwargs):
+        """
+        Plotting functionality from the hobs dictionary
+
+        Args:
+            obsname: (str) hobs package observation name
+            *args: matplotlib args
+            **kwargs: matplotlib kwargs
+
+        """
+
+        if obsname not in self:
+            raise AssertionError("Obsname {}: not valid".format(obsname))
+
+        axes = False
+        if 'ax' in kwargs:
+            ax = kwargs.pop('ax')
+            axes = True
+
+        obsval = self[obsname]['obsval']
+        simval = self[obsname]['simval']
+        date = self[obsname]['date']
+
+        kwargs['label'] = "Observed WL"
+        kwargs['color'] = 'r'
+        if axes:
+            ax.plot(date, obsval, *args, **kwargs)
+        else:
+            plt.plot(date, obsval, *args, **kwargs)
+
+        kwargs['label'] = "Simulated Head"
+        kwargs['color'] = 'b'
+        if axes:
+            ax.plot(date, simval, *args, **kwargs)
+        else:
+            plt.plot(date, simval, *args, **kwargs)
+
+        if axes:
+            return ax
+
+    def plot_measured_vs_simulated(self, filter=None, **kwargs):
+        """
+        Plots measured vs. simulated data along a 1:1 profile.
+
+        Args:
+            filter: (str, list, tuple, or function)
+                filtering criteria for writing statistics.
+                Function must return True for filter out, false for write to file
+            **kwargs: matplotlib.pyplot plotting kwargs
+
+        Returns:
+            axes: matplotlib axes object
+        """
+        axes = plt.subplot(111)
+
+        for obsname, meta_data in self.items():
+
+            if self.__filter(obsname, filter):
+                continue
+
+            simulated = meta_data['simval']
+            observed = meta_data['obsval']
+
+            axes.plot(observed, simulated, 'bo', markeredgecolor='k')
+
+
+        return axes
+
+    def plot_simulated_vs_residual(self, filter=None, **kwargs):
+        """
+        Creates a matplotlib plot of simulated heads vs residual
+
+        Args:
+            filter: (str, list, tuple, or function)
+                filtering criteria for writing statistics.
+                Function must return True for filter out, false for write to file
+            **kwargs: matplotlib.pyplot plotting kwargs
+
+        Returns:
+            axes: matplotlib axes object
+        """
+        axes = plt.subplot(111)
+
+        for obsname, meta_data in self.items():
+
+            if self.__filter(obsname, filter):
+                continue
+
+            residual = meta_data['residual']
+            observed = meta_data['obsval']
+
+            axes.plot(observed, residual, 'bo', markeredgecolor="k")
+
+        return axes
+
+
+if __name__ == "__main__":
+    ws = r'C:\Users\jlarsen\Desktop\Lucerne\Lucerne_OWHM\V0_initial_from_MODOPTIM\output'
+    hobsname = "hobs.out"
+
+    tmp = HobsOut(os.path.join(ws, hobsname))
+    tmp.plot("04N01W01R04S", "o-")
+    plt.legend(loc=0, numpoints=1)
+    plt.show()
+    print('break')
---
+++ hydmod_output.py
@@ -0,0 +1,336 @@
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+
+ITMUNI_LUT = {1: "seconds",
+              2: "minutes",
+              3: "hours",
+              4: "days",
+              5: "years"}
+
+COLORS = {i + 1: val for i, val in enumerate(["g", "y", "c", "saddlebrown",
+                                              "olivedrab", "m", "deeppink",
+                                              "maroon", "silver", "k"])}
+
+
+class BinaryData(object):
+    """
+    The BinaryData class is a class to that defines the data types for
+    integer, floating point, and character data in MODFLOW binary
+    files. The BinaryData class is the super class from which the
+    specific derived classes are formed.  This class should not be
+    instantiated directly.
+
+    """
+
+    def __init__(self):
+
+        self.integer = np.int32
+        self.integerbyte = self.integer(1).nbytes
+
+        self.character = np.uint8
+        self.textbyte = 1
+
+        return
+
+    def set_float(self, precision):
+        self.precision = precision
+        if precision.lower() == 'double':
+            self.real = np.float64
+            self.floattype = 'f8'
+        else:
+            self.real = np.float32
+            self.floattype = 'f4'
+        self.realbyte = self.real(1).nbytes
+        return
+
+    def read_text(self, nchar=20):
+        textvalue = self._read_values(self.character, nchar).tostring()
+        if not isinstance(textvalue, str):
+            textvalue = textvalue.decode().strip()
+        else:
+            textvalue = textvalue.strip()
+        return textvalue
+
+    def read_integer(self):
+        return self._read_values(self.integer, 1)[0]
+
+    def read_real(self):
+        return self._read_values(self.real, 1)[0]
+
+    def read_record(self, count, dtype=None):
+        if dtype is None:
+            dtype = self.dtype
+        return self._read_values(dtype, count)
+
+    def _read_values(self, dtype, count):
+        return np.fromfile(self.file, dtype, count)
+
+
+class HydModOut(BinaryData, dict):
+    """
+    A dictionary based Hydmod Binary observation reader.
+
+    Allows for sorting and acquiring observations based on ARR type,
+    location, layer, etc....
+
+    Inheits functionality from the FlopyBinaryData class which is reused
+    in this script as BinaryData(object)
+    """
+    def __init__(self, filename):
+
+        super(HydModOut, self).__init__()
+
+        self.name = filename
+        self.file = open(filename, 'rb')
+        self.nobs = self.read_integer()
+        self.totim = []
+        self.datetime = []
+
+        self.__get_precision()
+
+        self.itmuni = self.read_integer()
+
+        ctime = self.read_text(nchar=4)
+
+        # setup metadata for read and dictionary protocols
+        # maybe move this to its own hidden method to reduce clutter
+        self.__binary_hydlbl = []
+        self.__hydlbl_metadata = []
+        for ix in range(self.nobs):
+            lbl = self.read_text()
+            self.__set_dictionary(lbl)
+            self.__binary_hydlbl.append(lbl)
+
+        self.__build_dtype()
+        self.__read_data()
+        self.file.close()
+
+    @property
+    def hydlbl(self):
+        """
+        Returns a list of hydlbls, by using the built in dictionary keys method
+        """
+        return self.keys()
+
+    def __set_dictionary(self, lbl):
+        """
+        Takes the hydmod output label and uses it to set a dictionary entry
+        containing metadata for later use
+
+        Args:
+            lbl: (str) hydmod label
+
+        """
+        arr, intyp, layer, hydlbl = self.__split_label(lbl)
+
+        # todo: maybe break this part out for better clarity
+        self.__hydlbl_metadata.append((arr, intyp, layer, hydlbl))
+
+        if hydlbl in self:
+            self[hydlbl][layer] = {"arr": arr, "intyp": intyp, "data": []}
+        else:
+            self[hydlbl] = {layer: {"arr": arr, "intyp": intyp, "data": []}}
+
+    def __set_data_to_dict(self, record):
+        """
+        Parses the record read in from the hydmod file and appends data to
+        the appropriate dictionary location
+
+        Args:
+            record: numpy record of values
+        """
+        record = record[0]
+
+        self.totim.append(float(record[0]))
+        for ix, metadata in enumerate(self.__hydlbl_metadata):
+            hydlbl = metadata[-1]
+            klay = metadata[-2]
+            value = record[ix + 1]
+
+            self[hydlbl][klay]["data"].append(value)
+
+    def __split_label(self, lbl):
+        """
+        Splits the binary hydlbl and returns the Hydmod Arr, Intyp, KLay, and Hydlbl
+        values
+
+        Args:
+            lbl: (str) binary hydlbl
+
+        Returns:
+            Arr, Intyp, KLay, Hydlbl: (str, str, int, str)
+        """
+        arr = lbl[:2]
+        intyp = lbl[2]
+        layer = int(lbl[3:6])
+        hydlbl = lbl[6:]
+        return arr, intyp, layer, hydlbl
+
+    def __get_precision(self):
+        """
+        Sets the binary utility precision of floating point values
+        """
+        precision = 'single'
+        if self.nobs < 0:
+            self.nobs = abs(self.nobs)
+            precision = "double"
+        self.set_float(precision)
+
+
+    def __build_dtype(self):
+        """
+        Builds a data type record for the binary file reader to use in importing
+        data
+        """
+        dtype = [('totim', self.floattype)]
+        for site in self.__binary_hydlbl:
+            if not isinstance(site, str):
+                site_name = site.decode().strip()
+            else:
+                site_name = site.strip()
+            dtype.append((site_name, self.floattype))
+        self.dtype = np.dtype(dtype)
+
+    def __read_data(self):
+        """
+        Reads data in from the binary file
+        """
+        while True:
+            try:
+                r = self.read_record(count=1)
+                if r.size == 0:
+                    break
+                else:
+                    self.__set_data_to_dict(record=r)
+            except:
+                break
+        return
+
+    def date_time(self, start_date="1-1-1971", itmuni="days"):
+        """
+        Creates a date-time object from the totim array, sets this
+        list to self.datetime and returns it to the user
+
+        Args:
+            start_date: (str) start date for datetime object
+            itmuni: (str or int) can be text or corresponding modflow
+                itmunit number
+
+        Returns:
+            (list) datetime.datetime objects
+        """
+        mo, day, year = [int(i) for i in start_date.split('-')]
+        start_date = dt.datetime(year, mo, day)
+
+        if isinstance(itmuni, int):
+            itmuni = ITMUNI_LUT[itmuni]
+
+        itmuni = itmuni.lower()
+
+        if itmuni in ("seconds", "s", "sec", "secs", "second"):
+            self.datetime = [start_date + dt.timedelta(seconds=time) for
+                             time in self.totim]
+        elif itmuni in ("minutes", "m", "min", "mins", "minute"):
+            self.datetime = [start_date + dt.timedelta(minutes=time) for
+                             time in self.totim]
+        elif itmuni in ("hours", "hour", "h", "hrs", "hr"):
+            self.datetime = [start_date + dt.timedelta(hours=time) for
+                             time in self.totim]
+        elif itmuni in ("day", "d", "days"):
+            self.datetime = [start_date + dt.timedelta(days=time) for
+                             time in self.totim]
+        elif itmuni in ("year", "y", "years", "yr", "yrs"):
+            # todo: maybe find a more elegant solution, this is kind of hacky
+            self.datetime = [start_date + dt.timedelta(days=time*365.25) for
+                             time in self.totim]
+
+        return self.datetime
+
+
+    def plot(self, hydlbl, *args, **kwargs):
+        """
+        Returns a matplotlib axes object for layering in a plot.
+
+        Uses datetime if the user has called the get_date_time
+        method.
+
+        Args:
+            hydlbl: (str) hydmod label
+            *args: (matplotlib args)
+            **kwargs: (matplotlib kwargs) as well as a layer option
+        """
+
+        if hydlbl not in self:
+            raise AssertionError("hydlbl {}: not valid".format(hydlbl))
+
+        strip = False
+        if "strip_initial" in kwargs:
+            kwargs.pop("strip_initial")
+            strip = True
+
+        axes = False
+        if 'ax' in kwargs:
+            ax = kwargs.pop('ax')
+            axes = True
+
+        time = self.totim
+        if self.datetime:
+            time = self.datetime
+
+        if "layer" not in kwargs:
+
+            for lay, record in self[hydlbl].items():
+                kwargs["color"] = COLORS[lay]
+                kwargs['label'] = "Layer {} {}".format(lay, record['arr'])
+
+                if axes:
+                    if strip:
+                        ax.plot(time[1:], record["data"][1:], *args, **kwargs)
+                    else:
+                        ax.plot(time, record["data"], *args, **kwargs)
+                else:
+                    if strip:
+                        plt.plot(time[1:], record["data"][1:], *args, **kwargs)
+                    else:
+                        plt.plot(time, record["data"], *args, **kwargs)
+
+        else:
+            layer = kwargs.pop("layer")
+            if axes:
+                if strip:
+                    ax.plot(time[1:], self[hydlbl][layer]['data'][1:],
+                             *args, **kwargs)
+                else:
+                    ax.plot(time, self[hydlbl][layer]['data'], *args, **kwargs)
+            else:
+                if strip:
+                    plt.plot(time[1:], self[hydlbl][layer]['data'][1:],
+                             *args, **kwargs)
+                else:
+                    plt.plot(time, self[hydlbl][layer]['data'], *args, **kwargs)
+
+        if axes:
+            return ax
+
+
+if __name__ == "__main__":
+    ws = r'C:\Users\jlarsen\Desktop\Lucerne\Lucerne_OWHM\V0_initial_from_MODOPTIM\output'
+    hydname = "hydmod.out"
+
+    # hydmod = HydmodObs(os.path.join(ws, hydname))
+    # df = hydmod.get_dataframe(start_datetime='12-31-1941')
+
+    # headers = list(df)
+
+    # for header in headers:
+    #     df[header].plot()
+    #     plt.show()
+    tmp = HydModOut(os.path.join(ws, hydname))
+    tmp.date_time(start_date="12-31-1941")
+    print('break')
+    tmp.plot("04N01E05G02S", lw=2)
+    plt.legend(loc=0)
+    plt.ylim([2700, 2850])
+    plt.show()
